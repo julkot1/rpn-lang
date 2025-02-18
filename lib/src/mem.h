4034f6b273dadbf81abd52ccd169eb1f853c8e5e
julkot1 <julian.kotlowski@gmail.com>
2025-02-18 17:19:22 +0100

    fix struct copy in global scope

--- lib/src/mem.h
+++ lib/src/mem.h
@@ -2,4 +2,4 @@
 
 
 STC(CODE(255), NAME("copy"))
-void stc_memcpy(STC_I64 dest, STC_I64 src, STC_I64 n, STC_TYPE destT,  STC_TYPE srcT,  STC_TYPE nT);
+void stc_memcpy(STC_I64 dest, STC_I64 src, STC_I64 n);
